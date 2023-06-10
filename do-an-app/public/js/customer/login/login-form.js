cdb9cee3eec4da50da02041ca279467744d47c7f
ngovanthuan07 <ngovanthuan07@gmail.com>
2023-06-11 05:40:43 +0700

    update image 11_6_2023 t_40

--- do-an-app/public/js/customer/login/login-form.js
+++ do-an-app/public/js/customer/login/login-form.js
@@ -5,8 +5,17 @@ export function loginForm() {
             <div class="container">
                 <div class="card">
                   <button id="google-login" class="btn btn-lg btn-google btn-block text-uppercase btn-outline">
-                      <img src="https://img.icons8.com/color/16/000000/google-logo.png">
+                      <img width="30" height="30"  src="/media/logo/icons8-google-48.png">
                         Đăng nhập với GOOGLE
+                  </button>
+                </div>
+                <br>
+                <div>Hoặc</div>
+                <br>
+                <div class="card">
+                  <button id="facebook-login" class="btn btn-lg btn-facebook btn-block text-uppercase btn-outline">
+                      <img width="30" height="30" src="/media/logo/icons8-facebook-48.png">
+                        Đăng nhập với FACEBOOK
                   </button>
                 </div>
             </div>
@@ -18,10 +27,10 @@ export function loginForm() {
     });
 }
 
-export function handleLoginForm(newURL) {
+export function handleLoginForm(newURLOne, newURLTwo) {
     $(document).on('click', '#google-login', function () {
         $.ajax({
-            url: newURL,
+            url: newURLOne,
             type: 'GET',
             success: function(url) {
                 // Hiển thị cửa sổ đăng nhập Google
@@ -74,6 +83,66 @@ export function handleLoginForm(newURL) {
             },
             error: function() {
                 alert('Đăng nhập bằng Google thất bại oke.');
+            }
+        });
+    });
+
+
+    $(document).on('click', '#facebook-login', function () {
+        $.ajax({
+            url: newURLTwo,
+            type: 'GET',
+            success: function(url) {
+                // Hiển thị cửa sổ đăng nhập Google
+                const width = 500;
+                const height = 600;
+                const left = (window.innerWidth / 2) - (width / 2);
+                const top = (window.innerHeight / 2) - (height / 2);
+
+                let newWindow = window.open(url, '_blank', 'fullscreen=yes,location=no');;
+
+                // Chờ đợi cho đến khi người dùng đăng nhập xong và đóng cửa sổ đó
+                var timer = setInterval(function() {
+                    if (newWindow.closed) {
+                        clearInterval(timer);
+                        $.ajax({
+                            url: '/me',
+                            type: 'GET',
+                            success: function(resp) {
+                                let data = resp
+                                if(data) {
+                                    Swal.fire({
+                                        icon: "success",
+                                        title: "Thành công",
+                                        text: "Đăng nhập thành công!",
+                                        allowOutsideClick: false,
+                                    }).then((result) => {
+                                        if (result.isConfirmed) {
+                                            window.location.reload();
+                                        }
+                                    });
+                                }
+                            },
+                            error: function(err) {
+                                Swal.fire({
+                                    icon: "Error",
+                                    title: "Lỗi đăng nhập",
+                                    text: "Đã bị lỗi vui lòng đăng nhập lại!",
+                                    confirmButtonColor: "#d63031",
+                                    confirmButtonText: "Đóng",
+                                    allowOutsideClick: false,
+                                }).then((result) => {
+                                    if (result.isConfirmed) {
+                                        // window.location.reload();
+                                    }
+                                });
+                            }
+                        });
+                    }
+                }, 500);
+            },
+            error: function() {
+                alert('Đăng nhập bằng Facebook thất bại oke.');
             }
         });
     });
